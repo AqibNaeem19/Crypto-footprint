9cb3fbe2ada3e3d8d95c0cf6ec451f7f4f363968
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-03-17 13:41:03 +0500

    Make single file to import all components
    Index file imports all the components of the application. You can import any components or components to any directory by import from index file.
    Helpful in reducing tons of import statements in the parent components with just one statement to import all the required components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
-import Navbar from './components/Navbar' 
+import { Navbar } from './components/index' 
 
 const App = () => {
 	return (
