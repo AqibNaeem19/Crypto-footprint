d7cbc5bbff02099fc508e34c94e235aa207a72eb
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-03-18 11:34:53 +0500

    Added Markup of the home page
    Simply used the ant design classes to style the general structure of homepage with fake values. Later values will be fetched through some crypto API

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import millify from 'millify'
+import { Typography, Row, Col, Statistic } from 'antd'
+import { Link } from 'react-router-dom'
+
+const { Title } = Typography
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <React.Fragment>
+      <Title level={2} className="heading">Global Crypto Stats</Title>
+      <Row>
+        <Col span={12}><Statistic title="Cryptocurrencies" value="5" /></Col>
+        <Col span={12}><Statistic title="Exchanges" value="5" /></Col>
+        <Col span={12}><Statistic title="Market Cap" value="5" /></Col>
+        <Col span={12}><Statistic title="24h Volume" value="5" /></Col>
+        <Col span={12}><Statistic title="Total Markets" value="5" /></Col>
+      </Row>
+    </React.Fragment>
   )
 }
 
