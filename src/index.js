73cbf76ca063cb7bda0c45c57b91c2f8c756d4b0
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-03-18 16:08:20 +0500

    Configure redux in application
    Implement basic redux skeleton and created the store component.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import 'antd/dist/antd.min.css'
+import store from './app/store'
+import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <Router>
-    <App />
-  </Router>, 
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
   document.getElementById('root'));
---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,10 @@
+import Secretkey from "./secretKey"
+// var options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/exchange/-zdvbieRdZ',
+//   headers: {
+//     referencecurrencyuuid: 'yhjMzLPhuIDl',
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': SecretKey.value
+//   }
+// };
