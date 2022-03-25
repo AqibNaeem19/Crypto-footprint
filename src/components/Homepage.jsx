d3a593ff2489042e12865b7035bcb4ef7dd8ee87
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-03-25 21:24:17 +0500

    Home Page markup is complete
    On the home page there will be 3 or 4 sections about the crypto currencies. I have imported the required components and added a basic markup of these components.

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,13 +3,14 @@ import millify from 'millify'
 import { Typography, Row, Col, Statistic } from 'antd'
 import { useGetCryptosQuery } from '../services/cryptoApi'
 import { Link } from 'react-router-dom'
+import { Cryptocurrencies, News } from './index'
 
 const { Title } = Typography
 
 const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery();
   const globalStats = data?.data?.stats
-  if(isFetching) return 'Loading...'
+  if (isFetching) return 'Loading...'
   console.log(data)
   return (
     <React.Fragment>
@@ -21,6 +22,16 @@ const Homepage = () => {
         <Col span={12}><Statistic title="24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
         <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 Cryptocurrencies</Title>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
+      </div>
+      <Cryptocurrencies simplified/>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest Crypto News</Title>
+        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
+      </div>
+      <News simplified/>
     </React.Fragment>
   )
 }
