ed4dc89d532bba4e0f9066e1684ba45abc29ab57
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-03-26 16:41:50 +0500

    Fetching required number of Cryptocurrencies
    On the home page, i want to show only 10 coins instead of 100. But, if user clicks on the "show more" button. he/she can view top 100 coins instead of 10.
    Thats the purpose of limiting the required number of coins based on scenerio.

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -2,18 +2,21 @@ import React, { useState } from 'react'
 import millify from 'millify'
 import { Link } from 'react-router-dom'
 import { Card, Row, Col, Input } from 'antd'
-import { useGetCryptosQuery  } from '../services/cryptoApi'
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
-const Cryptocurrencies = () => {
-  const { data: cryptoList, isFetching } = useGetCryptosQuery();
-  const [ cryptos, setCryptos ] = useState(cryptoList?.data?.coins);
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
+  const [ cryptos, setCryptos ] =  useState(cryptoList?.data?.coins);
+
+  if(isFetching) return 'Loading..'
 
   return (
     <React.Fragment> 
       {/* gutter acts as grid spacing [horizontal, vertical] */}
       <Row gutter={[32, 32]} className="crypto-card-container">
 
-        {cryptos.map((currency) => (
+        {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
 
             <Link to={`/crypto/${currency.uuid}`}>
