645057c25d58274fe539ec324630e71e901dee36
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-03-26 17:11:46 +0500

    Added search functionality for Cryptocurrencies
    Now you can search the specific coin out of 100 coins

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import millify from 'millify'
 import { Link } from 'react-router-dom'
 import { Card, Row, Col, Input } from 'antd'
@@ -8,11 +8,20 @@ const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
   const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
   const [ cryptos, setCryptos ] =  useState(cryptoList?.data?.coins);
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    const filteredData = cryptoList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    setCryptos(filteredData)
+  }, [cryptoList, searchTerm])
 
   if(isFetching) return 'Loading..'
 
   return (
     <React.Fragment> 
+      <div className="search-crypto">
+        <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)}/>
+      </div>
       {/* gutter acts as grid spacing [horizontal, vertical] */}
       <Row gutter={[32, 32]} className="crypto-card-container">
 
